24c32c51ddf2bfa305b0e7df351b853dd9750b4e
BootCoder <bootcoder@gmail.com>
2018-01-07 14:39:00 -0800

    init store, actions, reducers

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,10 +1,40 @@
 import React, { Component, PropTypes } from 'react'
 
 class CoursesPage extends Component {
+  constructor (props, context) {
+    super(props, context)
+
+    this.state = {
+      course: { title: '' }
+    }
+
+    this.onTitleChange = this.onTitleChange.bind(this)
+    this.onClickSave = this.onClickSave.bind(this)
+  }
+
+  onTitleChange (event) {
+    const course = this.state.course
+    course.title = event.target.value
+    this.setState({course: course})
+  }
+
+  onClickSave () {
+    alert('did things')
+  }
+
   render () {
     return (
-      <div className="CoursesPage">
+      <div className='CoursesPage'>
         <h1>Courses</h1>
+        <h2>Add Course</h2>
+        <input
+          type='text'
+          onChange={this.onTitleChange}
+          value={this.state.course.title} />
+        <input
+          type='submit'
+          value='Save'
+          onClick={this.onClickSave} />
       </div>
     )
   }
